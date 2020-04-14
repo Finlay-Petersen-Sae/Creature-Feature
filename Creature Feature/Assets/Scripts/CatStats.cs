283a284894edf082c0a553562719b20e9f2c0004
Finlay-Petersen-Sae <1015492@student.sae.edu.au>
2020-04-14 12:43:12 +1000

    removed debug stuff

--- Creature Feature/Assets/Scripts/CatStats.cs
+++ Creature Feature/Assets/Scripts/CatStats.cs
@@ -7,7 +7,7 @@ public class CatStats : MonoBehaviour
     public int maxHealth, maxHunger, maxThirst, Ferocity, Sturdiness, maxCleansliness;
     public int curHealth, curHunger, curThirst, curCleansliness;
     public string catName;
-    public GameObject closestfood;
+    //public GameObject closestfood;
     //Insert an identifier of some sort for personality
     private void Start()
     {
@@ -17,24 +17,24 @@ public class CatStats : MonoBehaviour
         maxThirst = 100;
         maxCleansliness = 50;
         catName = PDM.catNames[Random.Range(0, PDM.catNames.Count)];
-        ClosestFood();
+        //ClosestFood();
     }
 
-    public void ClosestFood()
-    {
-        //TODO MAKE ONE FOR WATER TOO
-        float dist = 1000000f;
-        GameObject idealfood = null;
-        var PDM = FindObjectOfType<PathDataManager>();
-        foreach (var item in PDM.FoodObj)
-        {
-            if (Vector3.Distance(transform.position, item.transform.position) < dist)
-            {
+    //public void ClosestFood()
+    //{
+    //    //TODO MAKE ONE FOR WATER TOO
+    //    float dist = 1000000f;
+    //    GameObject idealfood = null;
+    //    var PDM = FindObjectOfType<PathDataManager>();
+    //    foreach (var item in PDM.FoodObj)
+    //    {
+    //        if (Vector3.Distance(transform.position, item.transform.position) < dist)
+    //        {
 
-                idealfood = item;
-                dist = Vector3.Distance(transform.position, item.transform.position);
-            }
-        }
-        closestfood = idealfood;
-    }
+    //            idealfood = item;
+    //            dist = Vector3.Distance(transform.position, item.transform.position);
+    //        }
+    //    }
+    //    closestfood = idealfood;
+    //}
 }
