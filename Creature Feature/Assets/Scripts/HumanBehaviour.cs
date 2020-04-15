970cb726cb4759087cd607e4a306a036d902e46b
Finlay-Petersen-Sae <1015492@student.sae.edu.au>
2020-04-15 13:58:46 +1000

    Made humans randomly spawn and despawn (will come into use later)

---
+++ Creature Feature/Assets/Scripts/HumanBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanBehaviour : MonoBehaviour
+{
+    private IEnumerator RanSpawn;
+    public GameObject humanObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        humanObj = this.transform.GetChild(0).gameObject;
+        StartCoroutine(TurnOnandOff());
+    }
+
+    private IEnumerator TurnOnandOff()
+    {
+        while (true)
+        {
+           //Debug.Log("coroutine active");
+            yield return new WaitForSecondsRealtime(Random.Range(5, 15));
+            if(humanObj.activeSelf)
+            {
+                humanObj.SetActive(false);
+                //Debug.Log("human set to inactive");
+            }
+            else if (!humanObj.activeSelf)
+            {
+                humanObj.SetActive(true);
+               //Debug.Log("human set to active");
+            }
+        }
+    }
+}
